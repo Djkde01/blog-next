cf3655844014e15800aaf66ef39bd367b0358f31
Djkde01 <sergio.estrella@utp.edu.co>
2022-10-06 01:24:05 -0500

    feat(Get post details & latests services)

--- src/services/index.js
+++ src/services/index.js
@@ -37,3 +37,40 @@ export const getPosts = async () => {
 
   return results.postsConnection.edges;
 };
+
+export const getFeaturedPosts = async () => {
+  const query = gql`
+    query GetPostDetails(){
+      posts(orderBy: createdAt_ASC last: 3){
+        title
+        featuredImage{
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]){
+      posts(where: { slug_not: $slug, AND { categories_some : {slug_in: $categories}}} last: 3){
+        title
+        featuredImage{
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
